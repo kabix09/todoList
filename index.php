9320e12c7b870b405c402d30fc3acbe29262995c
kabix09 <kabix.009@gmail.com>
2020-08-28 00:30:10 +0200

    add SQL query builder

--- index.php
+++ index.php
@@ -3,8 +3,16 @@ define('DB_CONFIG_FILE', __DIR__ . "/config/db.config.php");
 
 require_once __DIR__ . "/vendor/autoload.php";
 
-use App\Connection\Connection;
+use App\Connection\ {Connection, Finder};
 
 $conn = new Connection(include DB_CONFIG_FILE);
 $conn->connect();
-var_dump($conn->getConnection());
+
+$stmt = $conn->getConnection()->prepare(
+    Finder::select("user")::getSQL()
+);
+$stmt->execute();
+
+echo "<pre>";
+    var_dump($stmt->fetchAll(PDO::FETCH_ASSOC));
+echo "</pre>";
---
+++ src/Connection/Finder.php
@@ -0,0 +1,63 @@
+<?php declare(strict_types=1);
+namespace App\Connection;
+
+class Finder
+{
+    private static $classInstance = NULL;
+    private static $sql = "";
+    private static $prefix = "";
+    private static $where = array();
+    private static $suffix = "";
+
+    public static function select(string $table, array $columns = array()) : self {
+        self::$classInstance = new Finder();
+
+        self::$prefix = "SELECT";
+        if(!empty($columns)){
+            foreach ($columns as $columnName)
+                self::$prefix .= " " . $columnName . ",";
+
+            self::$prefix = substr(self::$prefix, 0, -1);
+        }else{
+            self::$prefix .= " *";
+        }
+        self::$prefix .= " FROM " . $table;
+
+        return self::$classInstance;
+    }
+
+    public static function where(?string $a = NULL) : self{
+        self::$where[0] = " WHERE " . $a;
+        return self::$classInstance;
+    }
+
+    public static function like(string $a, string $b) : self{
+        self::$where[] = trim($a . " LIKE " . $b);
+        return self::$classInstance;
+    }
+
+    public static function and(?string $a = NULL) : self{
+        self::$where[] = trim(" AND " . $a);
+        return self::$classInstance;
+    }
+
+    public static function or(?string $a = NULL) : self{
+        self::$where[] = trim(" OR " . $a);
+        return self::$classInstance;
+    }
+
+    public static function in(string $a, array $values) : self{
+        self::$where[] = trim($a . " IN (" . implode(" ", $values) . ")");
+        return self::$classInstance;
+    }
+
+    public static function orderBy(string $orderBy = "ASC"){
+        self::$suffix = trim("ORDER BY " . $orderBy);
+        return self::$classInstance;
+    }
+
+    public static function getSQL() : string {
+        self::$sql = self::$prefix . implode(" ", self::$where) . " " . self::$suffix;
+        return trim(self::$sql);
+    }
+}
