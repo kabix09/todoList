5809642520ee595baf801bc216ab3661ac53f972
kabix09 <kabix.009@gmail.com>
2020-09-14 19:41:57 +0200

    adjust task list refresh

--- index.php
+++ index.php
@@ -1,4 +1,29 @@
 <?php
+
+use App\Connection\Connection;
+use App\Repository\TaskRepository;
+
 require_once './init.php';
 
-include $_SESSION['ROOT_PATH'] . './templates/index.php';
+if(isset($_SESSION['user']) && $_SESSION['user']->getStatus() !== "active"){
+    header("Location: ./templates/accountStatus.php");
+    exit();
+}
+
+if(isset($_SESSION['user']))   //  && !isset($_SESSION['tasks'])
+{
+        // if exists remove old tasks
+    if(isset($_SESSION['tasks']))
+        unset($_SESSION['tasks']);
+
+        // download all tasks
+    $taskRepo = new TaskRepository(new Connection(include DB_CONFIG));
+    $tasks = $taskRepo->find(array(), [
+        "WHERE" => ["owner", "= '{$_SESSION['user']->getNick()}'"]
+    ]);
+
+    foreach ($tasks as $task)
+        $_SESSION['tasks'][] = $task;
+}
+
+include ROOT_PATH . './templates/index.php';
